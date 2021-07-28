4b08c996c0b1eb09a6a7317af3380a0f27de2348
Anjali Rohira <rohiraanjali@gmail.com>
2021-07-28 12:32:29 +0530

    refactored backend

--- react_server/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getQuestions, updateScoreBoard} = require("../controllers/Category.controller")
-
-router.route("/questions").get(getQuestions)
-
-router.route("/scoreboard/:quizID").post(updateScoreBoard)
-
-module.exports = router;
